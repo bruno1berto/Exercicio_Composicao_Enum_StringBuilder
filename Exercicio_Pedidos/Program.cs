44d3ce7c0377eb787797df653b572a2d840b5aa4
1bertoMaia <bruno1berto@hotmail.com>
2020-04-14 20:11:10 -0300

    Atualizando class program...

--- Exercicio_Pedidos/Program.cs
+++ Exercicio_Pedidos/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using Exercicio_Pedidos.Entities;
+using Exercicio_Pedidos.Entities.Enums;
 
 namespace Exercicio_Pedidos
 {
@@ -6,7 +8,27 @@ namespace Exercicio_Pedidos
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Enter Client data: ");
+            Console.WriteLine();
+            Console.Write("Name: ");
+            string name = Console.ReadLine();
+            Console.Write("Email: ");
+            string email = Console.ReadLine();
+            Console.Write("Birth date (DD/MM/YYYY): ");
+            DateTime birthdate = DateTime.Parse(Console.ReadLine());
+
+            Client c1 = new Client(name, email, birthdate);
+
+            Console.WriteLine();
+            Console.WriteLine("Enter Order data: ");
+            Console.WriteLine();
+
+            Console.Write("Status: ");
+            string orderstatus = Console.ReadLine();
+            Console.Write("How many items to this order? ");
+            int qtdItems = int.Parse(Console.ReadLine());
+
+            Order order = new Order(DateTime.Now, OrderStatus. orderstatus, c1);
         }
     }
 }
